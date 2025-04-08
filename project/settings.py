70689ff29a8ce76bdda3837f37babcc07bdf7c08
Karim Gamal Elsayed <kareemabolisa@gmail.com>
2025-04-08 09:28:53 +0200

    v3

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0c#=i!9z-y2x3afa0o(8==e+^82%_$82=+a(wt)tllf8*f@%=t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
