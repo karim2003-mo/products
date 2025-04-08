6391a98992cdb7544c72eb472d03e8767efb74e1
Karim Gamal Elsayed <kareemabolisa@gmail.com>
2025-04-08 09:20:19 +0200

    second commit

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import veiwproducts
 urlpatterns = [
-    
+    path('products/',veiwproducts, name='viewproducts')
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import Product
+def veiwproducts(request):
+    datalist=[]
+    products=Product.objects.all()
+    for product in products:
+        mp={
+            'name':product.name,
+            'price':product.price,
+            'category':product.category,
+            'rating':product.rating,
+            'image':product.image.url if product.image else None,  # Ensure image URL is valid
+            'description':product.description
+        }
+        datalist.append(mp)
+    return JsonResponse({"products":datalist})
 # Create your views here.
